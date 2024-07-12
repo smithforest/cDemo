8539b7a8db34b52b28a5e298e60ca7f755733780
黄森6202 <huangs@sciyon.com>
2024-07-12 13:38:21 +0800

    Initial commit

---
+++ common/common.h
@@ -0,0 +1,7 @@
+// 公共工具头文件
+#ifndef COMMON_H
+#define COMMON_H
+void pointerOutput(int number);
+void arrayOutPut(int* arr, int size);
+int* arrayGenerate(int size);
+#endif
---
+++ main.c
@@ -0,0 +1,7 @@
+#include "common\common.h"
+
+int main(void)
+{
+    int size=10;
+    arrayOutPut(arrayGenerate(size), size);
+}
